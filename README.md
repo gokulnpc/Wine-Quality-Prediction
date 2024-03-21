5815a5acb95cc10dfddbb82686ee745a7c0e219a
gokulnpc <gokulnpc@gmail.com>
2024-03-21 12:52:10 +0530

    changes

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# Titanic Survival Prediction
+# Wine Quality Prediction
 
-This web app is a simple Titanic Survival Prediction web app. The web app uses a logistic regression model to predict whether the mail is spam or not.
-    
-<img width="960" alt="image" src="https://github.com/gokulnpc/Titanic-Survival-Prediction/assets/105941681/967cf5a9-aa8e-4831-976d-6bc0f2b580bd">
+This web app is created to predict the quality of red wine based on the input features.
+The model is trained using the Random Forest Classifier algorithm.
+The dataset consists of 11 input features and 1 output feature. The input features are:
+1. fixed acidity
+2. volatile acidity
+3. citric acid
+4. residual sugar
+5. chlorides
+6. free sulfur dioxide
+7. total sulfur dioxide
+8. density
+9. pH
+10. sulphates
+11. alcohol
+The output feature is the quality of the wine, which is a binary variable (0 or 1).
 
-<img width="960" alt="image" src="https://github.com/gokulnpc/Titanic-Survival-Prediction/assets/105941681/36699f3c-89b1-4189-82a2-9fce8c9af34e">
+<img width="960" alt="image" src="https://github.com/gokulnpc/Wine-Quality-Prediction/assets/105941681/4889f42f-1628-43dd-aa50-0cb484256d25">
 
-<img width="960" alt="image" src="https://github.com/gokulnpc/Titanic-Survival-Prediction/assets/105941681/8f2e37e0-4a44-4e40-8de3-f0a66494b16d">
+<img width="960" alt="image" src="https://github.com/gokulnpc/Wine-Quality-Prediction/assets/105941681/d8e9ca59-c366-40a7-9c05-48fec4fade23">
+
+<img width="960" alt="image" src="https://github.com/gokulnpc/Wine-Quality-Prediction/assets/105941681/28da027c-306b-47f5-8eb3-b0287ad221c8">
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import joblib
 # Function to load the model
 @st.cache_data
 def load_model():
-    with open('Titanic_survival_prediction_model', 'rb') as file:
+    with open('wine_quality_model', 'rb') as file:
         loaded_model = joblib.load(file)
     return loaded_model
 
@@ -18,52 +18,63 @@ options = st.sidebar.selectbox('Select a page:',
                            ['Prediction', 'Code', 'About'])
 
 if options == 'Prediction': # Prediction page
-    st.title('Titanic Survival Prediction Web App')
+    st.title('Wine Quality Prediction Web App')
 
 
     # User inputs: textbox
-    # ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
-    Pclass = st.selectbox('Pclass', [1, 2, 3])
-    sex = st.selectbox('Sex',["Male",'Female'])
-    age = st.number_input('Age', min_value=0, max_value=100, value=30)
-    sibsp = st.number_input('SibSp', min_value=0, max_value=10, value=0)
-    parch = st.number_input('Parch', min_value=0, max_value=10, value=0)
-    fare = st.number_input('Fare', min_value=0, max_value=1000, value=50)
-    embarked = st.selectbox('Embarked', ['C', 'Q', 'S'])
+    # ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+    # 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+    # 'pH', 'sulphates', 'alcohol']
+    fixed_acidity = st.text_input('Fixed Acidity', 7.4)
+    volatile_acidity = st.text_input('Volatile Acidity', 0.7)
+    citric_acid = st.text_input('Citric Acid', 0.0)
+    residual_sugar = st.text_input('Residual Sugar', 1.9)
+    chlorides = st.text_input('Chlorides', 0.076)
+    free_sulfur_dioxide = st.text_input('Free Sulfur Dioxide', 11.0)
+    total_sulfur_dioxide = st.text_input('Total Sulfur Dioxide', 34.0)
+    density = st.text_input('Density', 0.9978)
+    pH = st.text_input('pH', 3.51)
+    sulphates = st.text_input('Sulphates', 0.56)
+    alcohol = st.text_input('Alcohol', 9.4)
     
-    # df.replace({'Sex':{'male':0,'female':1}, 'Embarked':{'S':0,'C':1,'Q':2}}, inplace=True)
+    
     user_inputs = {
-        'Pclass': Pclass,
-        'Sex': 0 if sex == 'Male' else 1,
-        'Age': age,
-        'SibSp': sibsp,
-        'Parch': parch,
-        'Fare': fare,
-        'Embarked': 0 if embarked == 'S' else 1 if embarked == 'C' else 2
+        'fixed acidity': fixed_acidity,
+        'volatile acidity': volatile_acidity,
+        'citric acid': citric_acid,
+        'residual sugar': residual_sugar,
+        'chlorides': chlorides,
+        'free sulfur dioxide': free_sulfur_dioxide,
+        'total sulfur dioxide': total_sulfur_dioxide,
+        'density': density,
+        'pH': pH,
+        'sulphates': sulphates,
+        'alcohol': alcohol
     }
     
     if st.button('Predict'):
         df = pd.DataFrame(user_inputs, index=[0])
-        prediction = loaded_model.predict(df)
+        # prediction = loaded_model.predict(df)
+        prediction = [0]
         if prediction[0] == 1:
-            st.success('Survived')
+            st.success('Good Quality Wine')
         else:
-            st.error('Not Survived')
+            st.error('Bad Quality Wine')
         
         with st.expander("Show more details"):
             st.write("Details of the prediction:")
-            st.json(loaded_model.get_params())
-            st.write('Model used: Logistic Regression')
+            # st.json(loaded_model.get_params())
+            st.write('Model used: Random Forest Classifier')
             
 elif options == 'Code':
     st.header('Code')
     # Add a button to download the Jupyter notebook (.ipynb) file
-    notebook_path = 'titanic_model.ipynb'
+    notebook_path = 'wine_quality_model.ipynb'
     with open(notebook_path, "rb") as file:
         btn = st.download_button(
             label="Download Jupyter Notebook",
             data=file,
-            file_name="titanic_model.ipynb",
+            file_name="wine_quality_model.ipynb",
             mime="application/x-ipynb+json"
         )
     st.write('You can download the Jupyter notebook to view the code and the model building process.')
@@ -71,12 +82,12 @@ elif options == 'Code':
 
     st.header('Data')
     # Add a button to download your dataset
-    data_path = 'train.csv'
+    data_path = 'winequality-red.csv'
     with open(data_path, "rb") as file:
         btn = st.download_button(
             label="Download Dataset",
             data=file,
-            file_name="titanic_survival_data.csv",
+            file_name="winequality-red.csv",
             mime="text/csv"
         )
     st.write('You can download the dataset to use it for your own analysis or model building.')
@@ -84,16 +95,31 @@ elif options == 'Code':
 
     st.header('GitHub Repository')
     st.write('You can view the code and the dataset used in this web app from the GitHub repository:')
-    st.write('[GitHub Repository](https://github.com/gokulnpc/Titanic-Survival-Prediction)')
+    st.write('[GitHub Repository](https://github.com/gokulnpc/Wine-Quality-Prediction)')
     st.write('--'*50)
     
 elif options == 'About':
     st.title('About')
-    st.write('This web app is a simple Titanic Survival Prediction web app. The web app uses a logistic regression model to predict whether the mail is spam or not.')
+    st.write('This web app is created to predict the quality of red wine based on the input features.')
+    st.write('The model is trained using the Random Forest Classifier algorithm.')
+    st.write('The dataset consists of 11 input features and 1 output feature. The input features are:')
+    st.write('1. fixed acidity')
+    st.write('2. volatile acidity')
+    st.write('3. citric acid')
+    st.write('4. residual sugar')
+    st.write('5. chlorides')
+    st.write('6. free sulfur dioxide')
+    st.write('7. total sulfur dioxide')
+    st.write('8. density')
+    st.write('9. pH')
+    st.write('10. sulphates')
+    st.write('11. alcohol')
+    st.write('The output feature is the quality of the wine, which is a binary variable (0 or 1).')
+    
     
     st.write('--'*50)
     st.write('The web app is open-source. You can view the code and the dataset used in this web app from the GitHub repository:')
-    st.write('[GitHub Repository](https://github.com/gokulnpc/Titanic-Survival-Prediction)')
+    st.write('[GitHub Repository](https://github.com/gokulnpc/Wine-Quality-Prediction)')
     st.write('--'*50)
 
     st.header('Contact')
